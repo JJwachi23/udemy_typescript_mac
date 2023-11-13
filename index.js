aaaa58a930977ce78a1ef949505aeacfc9539999
JJ <admin@admins-iMac.local>
2023-11-13 15:06:29 +0700

    Add: file tsconfig

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+"use strict";
 function add(n1, n2, showResult, phrase) {
     // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     //     throw new Error('Incorrect input!');
     // }
+    const result = n1 + n2;
     if (showResult) {
-        console.log(phrase + n1 + n2);
+        console.log(phrase + result);
     }
     else {
-        return n1 + n2;
+        return result;
     }
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 add(number1, number2, printResult, resultPhrase);
